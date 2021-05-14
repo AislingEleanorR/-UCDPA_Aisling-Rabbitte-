4b447943905153f3ed11a0964e7fe287797d624e
Aisling Rabbitte <aisling.rabbitte@rehabcare.ie>
2021-05-14 16:31:35 +0100

    "Sort DataSet by WHO Region"

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 data = pd.read_csv("country_wise_latest.csv")
+sorted = data.sort_values('WHO Region', ascending=True)
+print(sorted.columns)
 
-print(data.head())
+print(sorted.dtypes)
 
-print(data.tail())
+print(sorted.head())
+
+print(sorted.tail())
